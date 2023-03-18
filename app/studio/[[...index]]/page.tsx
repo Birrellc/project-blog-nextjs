959fa211865dd36ea057aa0e98759351ec9a98cb
birrellc <vaiezmedia@gmail.com>
2023-03-18 15:34:26 +0000

    Feat: added next-sanity routes

---
+++ app/studio/[[...index]]/page.tsx
@@ -0,0 +1,12 @@
+//https://www.npmjs.com/package/next-sanity?activeTab=readme#next-sanitypreview-live-real-time-preview
+
+'use client';
+
+import { NextStudio } from 'next-sanity/studio';
+
+import config from '../../../sanity.config';
+
+export default function StudioPage() {
+  //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
+  return <NextStudio config={config} />;
+}

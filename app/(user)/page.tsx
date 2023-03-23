09e2729f1eb1af9908be95967cfa43c15f2acb5f
birrellc <vaiezmedia@gmail.com>
2023-03-23 19:19:44 +0000

    Feat: added groq query search for blog posts on sanity cms

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,4 +1,18 @@
 import { previewData } from 'next/headers';
+import { groq } from 'next-sanity';
+
+// Sanity query
+// Get all of type post
+// select all fields
+// author and categories are references - categories is an array
+// expand the references and get data
+// order by created date
+
+const query = groq`*[_type == "post"] {
+  ...,
+  author->,
+  categories[]->,
+} | order(_createdAt desc)`;
 
 export default function Home() {
   // check if in preview mode

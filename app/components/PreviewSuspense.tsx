8bc199e573294cd108c8f3ce45e773376fd02b20
birrellc <vaiezmedia@gmail.com>
2023-03-25 18:31:19 +0000

    Feat: created previewSuspense (next-sanity work around)

---
+++ app/components/PreviewSuspense.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+// https://www.npmjs.com/package/next-sanity#next-sanitypreview-live-real-time-preview
+// Once rollup supports 'use client' module directives then 'next-sanity' will include them and this re-export will no longer be necessary
+export { PreviewSuspense as default } from 'next-sanity/preview';

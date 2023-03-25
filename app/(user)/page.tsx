807715470fd793529afacdae1ed1f2b07953e658
birrellc <vaiezmedia@gmail.com>
2023-03-25 13:46:33 +0000

    Test: fetch posts from sanity backend

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,5 +1,6 @@
 import { previewData } from 'next/headers';
 import { groq } from 'next-sanity';
+import { client } from '@/lib/sanity.client';
 
 // Sanity query
 // Get all of type post
@@ -8,16 +9,18 @@ import { groq } from 'next-sanity';
 // expand the references and get data
 // order by created date
 
-const query = groq`*[_type == "post"] {
+const query = groq`*[_type=="post"] {
   ...,
   author->,
   categories[]->,
 } | order(_createdAt desc)`;
 
-export default function Home() {
+export default async function Home() {
   // check if in preview mode
   if (previewData()) {
     return <div>Preview Mode</div>;
   }
+  const posts = await client.fetch(query);
+  console.log(posts);
   return <div>Not in preview mode</div>;
 }

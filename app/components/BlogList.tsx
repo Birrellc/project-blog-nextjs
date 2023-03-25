1cd58b435c9a24bed000f6a470ce8b9ca1cdc25a
birrellc <vaiezmedia@gmail.com>
2023-03-25 19:14:00 +0000

    Feat: rendered post images

--- app/components/BlogList.tsx
+++ app/components/BlogList.tsx
@@ -1,10 +1,34 @@
+import Image from 'next/image';
+import urlFor from '../../lib/urlFor';
+
 type Props = {
   posts: Post[];
 };
 
 const BlogList = ({ posts }: Props) => {
   console.log(posts.length);
-  return <div>BlogList</div>;
+  return (
+    <div>
+      <hr className='border-[#f77] mb-12' />
+      <div>
+        {/* posts */}
+        {posts.map((post) => (
+          <div key={post._id} className='group flex flex-col'>
+            <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-110 transition-transform duration-500 ease-out'>
+              {/* can change height etc on the cdn */}
+              {/* https://www.sanity.io/docs/image-url */}
+              {/* urlFor - helper function to convert url due to sanity image chaching */}
+              <Image
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogList;

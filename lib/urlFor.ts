8f1b08586f0ac575d749d29101088d955046f66a
birrellc <vaiezmedia@gmail.com>
2023-03-25 19:05:39 +0000

    Feat: created image helper funtion to access images from sanity backend

---
+++ lib/urlFor.ts
@@ -0,0 +1,14 @@
+// https://www.sanity.io/docs/image-url
+
+import { client } from './sanity.client';
+import imageUrlBuilder from '@sanity/image-url';
+
+// Get a pre-configured url-builder from your sanity client
+
+const builder = imageUrlBuilder(client);
+
+const urlFor = (source: any) => {
+  return builder.image(source);
+};
+
+export default urlFor;

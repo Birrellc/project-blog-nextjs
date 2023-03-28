8dfbfa3b81e179c7170ee5b01a2a898026cad031
birrellc <vaiezmedia@gmail.com>
2023-03-28 16:56:56 +0100

    Feat: created groq query to getch post data from sanity

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,10 +1,26 @@
+import { groq } from 'next-sanity';
+import { client } from '@/lib/sanity.client';
+
 type Props = {
   params: {
     slug: string;
   };
 };
 
-function Post({ params: { slug } }: Props) {
+async function Post({ params: { slug } }: Props) {
+  const query = groq`
+  // query if the type is post & slug is the same as the slug in the url get result back
+  *[_type == "post" && slug.current == $slug][0]{
+    // select all info from the post
+    ...,
+    // expand author and categories
+    author->,
+    categories[]->,
+  }
+  `;
+  // fetch params from the query / post data
+  const post: Post = await client.fetch(query, { slug });
+  console.log(post);
   return <div>Post: {slug}</div>;
 }
 
